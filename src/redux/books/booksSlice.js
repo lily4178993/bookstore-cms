2ce1175ad0cb75d98e64551ee6842121da696a60
lily4178993 <nellytelli@gmail.com>
2023-09-25 15:48:20 +0000

    feat: Implement category and book filtering feature
    This commit adds functionality to filter books by category and enhances the Redux slices for categories and books to support this feature. It also introduces a mechanism for selecting a category and displaying only books belonging to that category.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,17 +2,21 @@
  * Redux Slice for Book Data
  *
  * This Redux slice manages the state for book-related data, including an array of books.
- * It provides reducers for adding and removing books from the state.
+ * It provides reducers for adding and removing books from the state
+ and filtering books by category.
  *
  * @module booksSlice
  */
 
 import { createSlice } from '@reduxjs/toolkit';
 
+// Define the initial state with an empty array of books and filtered books
 const initialState = {
   books: [],
+  filteredBooks: [],
 };
 
+// Create a books slice
 const booksSlice = createSlice({
   name: 'books',
   initialState,
@@ -22,8 +26,8 @@ const booksSlice = createSlice({
      *
      * @function
      * @name addBook
+     * @param {Object} state - The current state of the books slice.
      * @param {Book} action.payload - The new book to add.
-     * @param {InitialState} state - The current state of the books slice.
      */
     addBook: (state, action) => {
       state.books.push(action.payload);
@@ -34,15 +38,44 @@ const booksSlice = createSlice({
      *
      * @function
      * @name removeBook
+     * @param {Object} state - The current state of the books slice.
      * @param {number} action.payload - The ID of the book to remove.
-     * @param {InitialState} state - The current state of the books slice.
      */
     removeBook: (state, action) => {
       state.books = state.books.filter((book) => book.id !== action.payload);
     },
+
+    /**
+     * Reducer to filter books by category.
+     *
+     * @function
+     * @name filteredBooksByCategory
+     * @param {Object} state - The current state of the books slice.
+     * @param {string|null} action.payload - The selected category
+     to filter by (or null to show all books).
+     */
+    filteredBooksByCategory: (state, action) => {
+      const selectedCategory = action.payload;
+      if (selectedCategory === null) {
+        state.filteredBooks = state.books;
+      } else {
+        state.filteredBooks = state.books.filter(
+          (book) => book.category === selectedCategory,
+        );
+      }
+    },
   },
 });
 
+/**
+ * Redux actions for the books slice.
+ *
+ * @typedef {Object} BooksActions
+ * @property {Function} addBook - Add a new book to the state.
+ * @property {Function} removeBook - Remove a book from the state by ID.
+ * @property {Function} filteredBooksByCategory - Filter books by category.
+ */
+
 // Export the reducer function and actions
-export const { addBook, removeBook } = booksSlice.actions;
+export const { addBook, filteredBooksByCategory, removeBook } = booksSlice.actions;
 export default booksSlice.reducer;
