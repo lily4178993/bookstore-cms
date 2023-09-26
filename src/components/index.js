9938d8b60c2708d27a16d08e913e4a36ffe31226
lily4178993 <nellytelli@gmail.com>
2023-09-26 13:28:56 +0000

    feat(containers): Refactor Categories component to include category filtering functionality

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as BookForm } from './BookForm';
 export { default as BookList } from './BookList';
-export { default as Categories } from './Categories';
 export { default as Menu } from './Menu';
