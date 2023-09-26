0db28f7f8b61737e4696626865a728f64d77664e
lily4178993 <nellytelli@gmail.com>
2023-09-26 13:18:08 +0000

    feat(data): Add 'category' attribute to initialBooksData

--- src/constants/initialBooksData.js
+++ src/constants/initialBooksData.js
@@ -1,15 +1,19 @@
 /**
  * initialBooksData - Represents an array of initial book data.
  *
- * This array contains an initial set of book objects with id, author, and title properties.
+ * This array contains an initial set of book objects with id, author, title,
+ * and category properties.
  * It is typically used as the initial data source for displaying books in the application.
+ *
+ * @type {Array.<object>}
  */
+
 const initialBooksData = [
   {
     id: 1,
     author: 'Author 01',
     title: 'Title 01',
-    category: 'Action',
+    category: 'Fiction',
   },
   {
     id: 2,
