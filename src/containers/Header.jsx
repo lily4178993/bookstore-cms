2ccface2b13d96648580ccd4dda5a382f7c0524e
lily4178993 <nellytelli@gmail.com>
2023-09-29 19:50:19 +0000

    style: Update Header component styles and implement useLocation hook
    This commit refines the styling of the Header component, ensuring a consistent and visually appealing appearance. It also introduces the use of the useLocation hook to dynamically set the active link based on the current URL. The active link is visually highlighted with the ont-bold class for improved user navigation.
    
    - Enhanced the visual design of the Header component.
    - Implemented the useLocation hook to determine the active link.
    - Applied the ont-bold class to the active link for better user feedback.

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import userIcon from '../assets/images/icons8-user.png';
 
 /**
@@ -11,38 +11,64 @@ import userIcon from '../assets/images/icons8-user.png';
  * @component
  */
 
-const Header = () => (
-  <header className="flex items-center justify-between gap-1">
-    <span className="text-2xl">Bookstore CMS</span>
-    <ul className="flex gap-2">
-      <li>
-        <Link
-          to="/"
-          className="px-3 py-2"
-        >
-          Books
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/categories/All"
-          className="px-3 py-2"
-        >
-          Categories
-        </Link>
-      </li>
-    </ul>
-    <div
-      type="button"
-      className="justify-self-end p-4 rounded-full"
-    >
-      <img
-        src={userIcon}
-        alt="user icon"
-        className="w-8 h-8"
-      />
-    </div>
-  </header>
-);
+const Header = () => {
+  const location = useLocation();
 
+  // State to track the active link based on the current URL
+  const [activeLink, setActiveLink] = useState('');
+
+  /**
+   * React useEffect hook to determine the active link based on the current URL.
+   * The active link is used to apply the "font-bold" class to the active link.
+   *
+   * @function
+   * @name useEffect
+   * @param {Array} [location.pathname] - An array containing the current pathname
+   * from the location object.
+   */
+  useEffect(() => {
+    // Determine which link is active based on the current URL
+    if (location.pathname === '/') {
+      setActiveLink('books');
+    } else if (location.pathname === '/categories/All') {
+      setActiveLink('categories');
+    }
+  }, [location.pathname]);
+
+  return (
+    <header className="relative flex items-end justify-between gap-5 px-4 pt-8 pb-2 shadow-sm md:px-10 md:py-4 xl:px-[8%] xl:shadow-md">
+      <div className="flex gap-5">
+        <span className="text-xl text-secondary font-bold absolute top-1 left-0 right-0 text-center md:text-2xl md:relative">Bookstore CMS</span>
+        <ul className="flex items-center md:items-end">
+          <li>
+            <Link
+              to="/"
+              className={`text-xs uppercase px-2 py-2 ${activeLink === 'books' ? 'font-bold' : 'opacity-50'}`}
+            >
+              Books
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/categories/All"
+              className={`text-xs uppercase px-2 py-2 ${activeLink === 'categories' ? 'font-bold' : 'opacity-50'}`}
+            >
+              Categories
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div
+        type="button"
+        className="h-6 p-1 rounded-full border-2 md:h-8"
+      >
+        <img
+          src={userIcon}
+          alt="user icon"
+          className="w-full h-full"
+        />
+      </div>
+    </header>
+  );
+};
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#307bbe',
+        secondary: '#0290ff',
+        ternary: '#e8e8e8',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
